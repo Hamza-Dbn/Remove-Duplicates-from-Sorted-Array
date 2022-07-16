f6c253dbd572e00ffd4f02507e1b6c2918dd4a1e
Hamza Darbani <76852492+Hamza-Dbn@users.noreply.github.com>
2022-07-16 14:18:39 +0200

    Wrong solution

--- code.js
+++ code.js
@@ -3,17 +3,5 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    const result = [];
-    let k = result.length;
-    const hashtable = [];
-    for (num of nums) {
-        if (hashtable[num] == null) {
-            hashtable[num] == true; 
-            result.push(num);
-        }
-        else {
-            continue;
-        }   
-    }
-return k, result;
+
 };
