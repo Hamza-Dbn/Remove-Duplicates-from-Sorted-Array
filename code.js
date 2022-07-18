58736bf12bb55d08af3fae68a0afec40eeb6b016
Hamza Darbani <76852492+Hamza-Dbn@users.noreply.github.com>
2022-07-19 01:13:17 +0200

    Update code.js

--- code.js
+++ code.js
@@ -3,5 +3,12 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-
+  let Index = 1;
+  for (let i = 0; i < nums.length - 1; i++) {
+    if(nums[i] !== nums[i+1]) {
+       nums[Index] = nums[i+1];
+       Index ++;
+       } 
+  }
+  return Index;
 };
